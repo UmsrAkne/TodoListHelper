76e767607f89e702740f1602783b6a26964256de
Akn <sibata486@gmail.com>
2023-05-18 18:01:11 +0900

    add feat / テキストから Todo のリストを生成する機能を実装

--- TestTodoListHelper/Models/TestParser.cs
+++ TestTodoListHelper/Models/TestParser.cs
@@ -1,9 +1,30 @@
 using NUnit.Framework;
+using TodoListHelper.Models;
 
 namespace TestTodoListHelper.Models
 {
     [TestFixture]
     public class TestParser
     {
+        private readonly string sampleText =
+            "20230518 ------------------------------------" +
+            "[ ] group1 / group2 / Sample todo 1 / 30min\n" +
+            "\n" +
+            "[ ] group1 / group2 / Sample todo 2 / 30min\n" +
+            "\tdescription text\n" +
+            "\n" +
+            "[x] group1 / group2 / Sample todo 3 / 30min\n";
+
+        [Test]
+        public void GetTodoListTest()
+        {
+            var parser = new Parser();
+
+            var todos = parser.GetTodoList(sampleText);
+            Assert.AreEqual(3, todos.Count);
+            Assert.AreEqual(todos[0], "[ ] group1 / group2 / Sample todo 1 / 30min\n\n");
+            Assert.AreEqual(todos[1], "[ ] group1 / group2 / Sample todo 2 / 30min\n\tdescription text\n\n");
+            Assert.AreEqual(todos[2], "[x] group1 / group2 / Sample todo 3 / 30min\n");
+        }
     }
 }
--- TodoListHelper/Models/Parser.cs
+++ TodoListHelper/Models/Parser.cs
@@ -1,6 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace TodoListHelper.Models
 {
     public class Parser
     {
+        /// <summary>
+        /// 入力したテキストからパース可能な全ての Todo と、それに付随するテキストをリストにして取得します。
+        /// </summary>
+        /// <param name="text">フォーマットに沿って記述された Todoリスト</param>
+        /// <returns>角括弧を基準に分割された Todo のリスト</returns>
+        public List<string> GetTodoList(string text)
+        {
+            var rep = Regex.Replace(text, "\\[.\\]", ",,,,$0");
+            return Regex.Split(rep, ",,,,").Where(s => Regex.IsMatch(s, "\\[.\\]")).ToList();
+        }
     }
 }
